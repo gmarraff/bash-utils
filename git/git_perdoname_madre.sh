bdb8a5768d73ef389bc6fb729820bf6050b6e2f8
gmarraff <gianluca.marraffa@sysdig.com>
2022-02-08 14:52:49 +0100

    Add git_perdoname_madre.sh

---
+++ git/git_perdoname_madre.sh
@@ -0,0 +1,15 @@
+git_perdoname_madre_forced() {
+    git add .
+    git commit --amend --no-edit
+    git push -f
+}
+
+git_perdoname_madre() {
+    echo "You are about to force amending and force pushing the commit."
+    read -p "Continue [y/N]? " answer
+    if [[ "$answer" == "y" || "$answer" == "Y" || "$answer" == "yes" ]]; then
+        git_perdoname_madre
+    else
+        echo "$USERNAME said no, aborted."
+    fi
+}
