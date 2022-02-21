05671248acc6b3a22e9345867ddfcb91cc9b1d64
gmarraff <gianluca.marraffa@sysdig.com>
2022-02-21 14:43:54 +0100

    Fix entrypoint.sh sourcing
    'source' only accept one argument, with this change the sourcing
    is properly done with all the files in the directory.
    The entrypoint is also structured to make it easy to add new
    directories.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #/!bin/bash
 
-source $(dirname $BASH_SOURCE)/git/*.sh
+WORKDIR=$(dirname $BASH_SOURCE)
+DIRECTORIES="
+git
+"
+
+for dir in $DIRECTORIES; do
+    for file in  $WORKDIR/$dir/*.sh; do
+        source $file
+    done
+done
