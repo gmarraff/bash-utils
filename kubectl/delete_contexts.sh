3981002a2f61d345c170cec9020480feed8b7eab
gmarraff <gianluca.marraffa@sysdig.com>
2024-02-08 12:29:23 +0100

    [kubectl] Add helm_delete_releases

--- kubectl/delete_contexts.sh
+++ kubectl/delete_contexts.sh
@@ -16,3 +16,21 @@ kubectl_delete_contexts() {
         for context in $contexts; do kubectl config delete-context $context; done
     fi
 }
+
+helm_delete_releases() {
+    namespace=${1:-default}
+    pattern=${2:-.+}
+
+    releases=$(helm -n $namespace list | grep -v NAME | awk '{print $1}' | grep -E "$pattern")
+    if [[ $releases == "" ]]; then
+        echo "No releases found in namespace $namespace."
+        return
+    fi
+
+    echo -e "You are about to delete the following releases:\n"
+    echo $releases | tr " " "\n"
+
+    if user_confirm; then
+        echo $releases | tr " " "\n" | xargs helm uninstall
+    fi
+}
